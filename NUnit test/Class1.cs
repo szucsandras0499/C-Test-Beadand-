2302929d58547e480f08bc998f0f06e119b64be2
Petrik Lajos Szg <info@petrik.hu>
2019-03-04 16:28:39 +0100

    Első verzió a beadandónak.

---
+++ NUnit test/Class1.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NUnit_test
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        JaratKezelo j;
+
+        [SetUp]
+        public void Setup()
+        {
+            j = new JaratKezelo();
+        }
+        
+        [Test]
+        public void UjJarat() {
+            j.UjJarat("123", "Debrecen", "Tokió", new DateTime(2019, 3, 4, 14, 30, 05));
+
+         }
+
+        [Test]
+        public void Keses() {
+            j.UjJarat("123", "Debrecen", "Tokió", new DateTime(2019, 3, 4, 14, 30, 05));
+            j.Keses("123", 10);
+            Assert.AreEqual(new DateTime(2019, 3, 4, 14, 40, 05), j.MikorIndul("123"));
+        }
+        [Test]
+        public void Keses2()
+        {
+            j.UjJarat("123", "Debrecen", "Tokió", new DateTime(2019, 3, 4, 14, 30, 05));
+            Assert.Throws<JaratKezelo.NegativKesesException>(
+                () => {
+                    j.Keses("123", -25);
+                    //                    j.UjJarat("123", "Debrecen", "Tokió", new DateTime(2019, 3, 4, 14, 30, 05));
+                }
+                );
+            
+        }
+
+        [Test]
+        public void MikorIndul() {
+            j.UjJarat("123", "Debrecen", "Tokió", new DateTime(2019, 3, 4, 14, 30, 05));
+            Assert.AreEqual(new DateTime(2019, 3, 4, 14, 30, 05), j.MikorIndul("123"));
+        }
+
+       
+
+        [Test]
+        public void JaratokRepuloterrol() {
+            List<string> ujjarat = new List<string>();
+            ujjarat.Add("123");
+            j.UjJarat("123", "Debrecen", "Tokió", new DateTime(2019, 3, 4, 14, 30, 05));
+            Assert.AreEqual(ujjarat, j.JaratokRepuloterrol("Debrecen"));
+        }
+
+
+        [Test]
+        public void JaratokRepuloterrol2()
+        {
+
+            Assert.Throws<JaratKezelo.JaratokRepuloterrolException>(
+                 () => {
+                     j.JaratokRepuloterrol("Pécs");
+                    //                    j.UjJarat("123", "Debrecen", "Tokió", new DateTime(2019, 3, 4, 14, 30, 05));
+                }
+                 );
+        }
+    }
+
+}
---
+++ NUnit test/JaratKezelo.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_test
+{
+    class JaratKezelo
+    {
+        class Jarat
+        {
+            public string jaratSzam { get; set; }
+            public string repterHonnan { get; set; }
+            public string repterHova { get; set; }
+            public DateTime indulas { get; set; }
+            public int keses { get; set; }
+
+            public Jarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+            {
+                this.jaratSzam = jaratSzam;
+                this.repterHonnan = repterHonnan;
+                this.repterHova = repterHova;
+                this.indulas = indulas;
+                this.keses = 0;
+            }
+            
+        }
+        List<Jarat> jaratok = new List<Jarat>();
+
+        
+        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas) {
+            var j = new Jarat(jaratSzam, repterHonnan, repterHova, indulas);
+            jaratok.Add(j);
+        }
+
+        public void Keses(string jaratSzam, int keses) {
+            foreach (var e in jaratok) {
+                if (jaratSzam.Equals(e.jaratSzam)) {
+                    if ((e.keses + keses) < 0)
+                    {
+                        throw new NegativKesesException(keses);
+                    }
+                    else {
+                        e.keses = keses;
+                    }
+                }
+            }
+        }
+
+       public  class NegativKesesException : Exception {
+            public NegativKesesException(int keses) : base("Baj van!" + keses)
+            { }
+        }
+
+        public DateTime MikorIndul(string jaratSzam) {
+            foreach (var gep in jaratok)
+            {
+                if (jaratSzam.Equals(gep.jaratSzam)) {
+                    return gep.indulas.AddMinutes(gep.keses);
+                }
+
+            }
+            throw new ArgumentException("Ilyen járat nem létezik!");
+        }
+
+        public class JaratokRepuloterrolException : Exception
+        {
+            public JaratokRepuloterrolException(string repterHonnan) : base("Baj van repülőtér!" + repterHonnan)
+            { }
+        }
+
+        public List<string> JaratokRepuloterrol(string repter) {
+            List<string> jaratokLista = new List<string>();
+            foreach (var e in jaratok) {
+                if (repter.Equals(e.repterHonnan)) {
+                    jaratokLista.Add(e.jaratSzam);
+                }
+            }
+            if (jaratokLista.Count == 0) {
+                throw new JaratokRepuloterrolException(repter);
+            }
+            return jaratokLista;
+        }
+
+    }
+}
